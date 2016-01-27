36d34b5c015fb4faa15104e306c9cf2672a5f0ff
SangHyeon <sktkddusrj@naver.com>
2016-01-27 18:20:02 +0900

    test server

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('112.108.39.164', 8080)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+sock.listen(1)
+
+while True:
+    print >>sys.stderr, 'watiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        while True:
+            data = connection.recv(16)
+            print >>sys.stderr, 'received "%s"' % data
+            if data:
+                print >>sys.stderr, 'sending data back to the client'
+                connection.sendall(data)
+            else:
+                print >>sys.stderr, 'no more data from', client_address
+                break
+    finally:
+        connection.close()
+
+
